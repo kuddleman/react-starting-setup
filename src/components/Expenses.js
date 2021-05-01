9dd2624f021c957f58361cbb3cf3b92f1708978c
Kuddleman <edl1155@gmail.com>
2021-05-01 15:20:59 -0700

    add css

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
 import ExpenseItem from './ExpenseItem'
+import Card from './Card'
 import './Expenses.css'
 
 function Expenses(props) {
 
     return (
-        <div className="expenses">
+        <Card className="expenses">
         <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -24,7 +25,7 @@ function Expenses(props) {
         date={props.items[3].date}
       />
 
-        </div>
+        </Card>
     )
 
 }
