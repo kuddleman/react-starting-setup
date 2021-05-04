1babab747ab5a59be7c9e55bb8bdde69d7546a2f
Kuddleman <edl1155@gmail.com>
2021-05-03 18:08:25 -0700

    add state for title, amount and date

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,12 @@
+import ExpenseForm from './ExpenseForm'
+
+import './NewExpense.css'
+
 const NewExpense = () => {
 
     return (
         <div className="new-expense">
-            
+           <ExpenseForm /> 
         </div>
     )
 
